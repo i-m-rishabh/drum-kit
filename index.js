a134c6f5efa7e9fad0cc002d7b9bf19f003ab73d
i-m-rishabh <rishabhpandey12798@gmail.com>
2022-10-10 20:27:21 +0530

    responsive key press

--- index.js
+++ index.js
@@ -4,49 +4,49 @@ function buttonClick ( x ){
         case 'w':
           // code block
           new Audio("sounds/crash.mp3").play();
-          keyDown();
+          
           keyPress(".w-drum");
           
           break;
         case 'a':
           // code block
           new Audio("sounds/kick-bass.mp3").play();
-          keyDown();
+        //   keyDown();
           keyPress(".a-drum");
           
           break;
         case 's':
           // code block
           new Audio("sounds/snare.mp3").play();
-          keyDown();
+        //   keyDown();
           keyPress(".s-drum");
           
           break;
         case 'd':
             // code block
             new Audio("sounds/tom-1.mp3").play();
-            keyDown();
+            // keyDown();
             keyPress(".d-drum");
             
             break;
         case 'j':
                 // code block
                 new Audio("sounds/tom-2.mp3").play();
-                keyDown();
+                // keyDown();
                 keyPress(".j-drum");
                 
             break;
         case 'k':
                     // code block
             new Audio("sounds/tom-3.mp3").play();
-            keyDown();
+            // keyDown();
             keyPress(".k-drum");
             
             break;
         case 'l':
                 // code block
             new Audio("sounds/tom-4.mp3").play();
-            keyDown();
+            // keyDown();
             keyPress(".l-drum");
             
             break;
@@ -62,6 +62,12 @@ document.addEventListener('keydown', (event) => {
     buttonClick(name);
   }, false);
 
+  document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    keyDown();
+  }, false);
 function keyPress(x){
     document.querySelector(x).style.border = "5px solid blue";
 }

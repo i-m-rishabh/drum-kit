91fd4e6253ef944640033e7f9bd507d8b84bb672
i-m-rishabh <rishabhpandey12798@gmail.com>
2022-10-11 07:10:20 +0530

    refactoring previous code

--- index.js
+++ index.js
@@ -5,49 +5,49 @@ function buttonClick ( x ){
           // code block
           new Audio("sounds/crash.mp3").play();
           
-          keyPress(".w-drum");
+          keyPress(".w");
           
           break;
         case 'a':
           // code block
           new Audio("sounds/kick-bass.mp3").play();
         //   keyDown();
-          keyPress(".a-drum");
+          keyPress(".a");
           
           break;
         case 's':
           // code block
           new Audio("sounds/snare.mp3").play();
         //   keyDown();
-          keyPress(".s-drum");
+          keyPress(".s");
           
           break;
         case 'd':
             // code block
             new Audio("sounds/tom-1.mp3").play();
             // keyDown();
-            keyPress(".d-drum");
+            keyPress(".d");
             
             break;
         case 'j':
                 // code block
                 new Audio("sounds/tom-2.mp3").play();
                 // keyDown();
-                keyPress(".j-drum");
+                keyPress(".j");
                 
             break;
         case 'k':
                     // code block
             new Audio("sounds/tom-3.mp3").play();
             // keyDown();
-            keyPress(".k-drum");
+            keyPress(".k");
             
             break;
         case 'l':
                 // code block
             new Audio("sounds/tom-4.mp3").play();
             // keyDown();
-            keyPress(".l-drum");
+            keyPress(".l");
             
             break;
         
@@ -57,7 +57,7 @@ function buttonClick ( x ){
 }
 document.addEventListener('keydown', (event) => {
     var name = event.key;
-    var code = event.code;
+    // var code = event.code;
     // Alert the key name and key code on keydown
     buttonClick(name);
   }, false);
@@ -72,13 +72,18 @@ function keyPress(x){
     document.querySelector(x).style.border = "5px solid blue";
 }
 function keyDown(){
-    document.querySelectorAll("button")[0].style.border = "5px solid rgba(26, 109, 91, 0.991)";
-    document.querySelectorAll("button")[1].style.border = "5px solid rgba(26, 109, 91, 0.991)";    
-    document.querySelectorAll("button")[2].style.border = "5px solid rgba(26, 109, 91, 0.991)";   
-    document.querySelectorAll("button")[3].style.border = "5px solid rgba(26, 109, 91, 0.991)";
-    document.querySelectorAll("button")[4].style.border = "5px solid rgba(26, 109, 91, 0.991)";
-    document.querySelectorAll("button")[5].style.border = "5px solid rgba(26, 109, 91, 0.991)";
-    document.querySelectorAll("button")[6].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+    var n = document.querySelectorAll(".drum").length;
+    for (let index = 0; index < n; index++) {
+      document.querySelectorAll(".drum")[index].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+      
+    }
+    // document.querySelectorAll("button")[0].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+    // document.querySelectorAll("button")[1].style.border = "5px solid rgba(26, 109, 91, 0.991)";    
+    // document.querySelectorAll("button")[2].style.border = "5px solid rgba(26, 109, 91, 0.991)";   
+    // document.querySelectorAll("button")[3].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+    // document.querySelectorAll("button")[4].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+    // document.querySelectorAll("button")[5].style.border = "5px solid rgba(26, 109, 91, 0.991)";
+    // document.querySelectorAll("button")[6].style.border = "5px solid rgba(26, 109, 91, 0.991)";
 
 
 
